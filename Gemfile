2d3921b0d063ee980c30ace49e10cda6bf688acc
Conde Morales <jpmorales@miuandes.cl>
2024-08-07 18:11:18 -0400

    seeds and style

--- Gemfile
+++ Gemfile
@@ -2,6 +2,10 @@ source "https://rubygems.org"
 
 ruby "3.3.4"
 
+
+gem 'faker'
+
+
 # gem 'pg'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,49 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+50.times do
+  Author.create(
+    name: Faker::Book.author,
+    date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
+    country_of_origin: Faker::Address.country,
+    description: Faker::Lorem.paragraph(sentence_count: 3)
+  )
+end
+
+author_ids = Author.pluck(:id)
+
+300.times do
+  Book.create(
+    name: Faker::Book.title,
+    summary: Faker::Lorem.paragraph(sentence_count: 5),
+    publication_date: Faker::Date.backward(days: 365 * 5), 
+    number_of_sales: Faker::Number.between(from: 0, to: 10000),
+    author_id: author_ids.sample
+  )
+end
+
+Book.find_each do |book|
+    rand(1..10).times do
+      Review.create(
+        book: book,
+        review: Faker::Lorem.paragraph(sentence_count: 3),
+        score: Faker::Number.between(from: 1, to: 5),
+        up_votes: Faker::Number.between(from: 0, to: 100)
+      )
+    end
+  end
+  
+  Book.find_each do |book|
+   
+    (0..4).each do |i|
+      year = Date.today.year - i
+      Sale.create(
+        book: book,
+        year: year,
+        sales: Faker::Number.between(from: 0, to: 10000) 
+      )
+    end
+  end
